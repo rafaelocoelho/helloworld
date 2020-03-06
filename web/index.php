b9e1db9b5be095f1aaa47ac78be9f358303015e9
Rafael de O Coelho <rafael.oliveiracoelho@gmail.com>
2020-03-06 11:15:05 -0300

    Ajuste

--- web/index.php
+++ web/index.php
@@ -18,7 +18,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app->get('/', function () {
-    return 'Hello World';
+    return 'Hello World!';
 });
 $app->get('/goodbye', function () {
     return 'Goodbye World';
